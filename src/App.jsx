b366cdfd29488dd7b30288a9e303af346d196a34
Jason Woo <jaswoo73@gmail.com>
2025-01-13 16:10:19 +0800

    Fixed typo in imports of App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import About from "./components/About";
 import Contact from "./components/Contact";
 import Features from "./components/Features";
 import Footer from "./components/Footer";
-import Hero from "./components/hero";
+import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import Story from "./components/Story";
 
